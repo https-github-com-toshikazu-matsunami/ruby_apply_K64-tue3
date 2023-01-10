e3838239b5adab95bfa529113646ed15ac56005e
tuan-anh-mk1 <trananh16112001@gmail.com>
2023-01-10 23:03:39 +0700

    kadai 2,3,4

---
+++ kadai2.rb
@@ -0,0 +1,23 @@
+class Menu
+    attr_accessor :name
+    attr_accessor :price
+  
+    def initialize(name, price)
+      self.name = name
+      self.price = price
+    end
+  
+    def show_data
+      puts "#{self.name} #{self.price}vnd"
+    end
+  end
+
+  menu1 = Menu.new("Phở", 30_000)
+  menu2 = Menu.new("Bún chả", 40_000)
+  menu3 = Menu.new("Bánh mì", 20_000)
+  
+  menus = Array[menu1, menu2, menu3]
+
+  menus.each {
+    |menu| menu.show_data
+  }
